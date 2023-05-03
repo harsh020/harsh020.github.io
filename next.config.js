a4e73e1b85c0e8c131c7d50fa5122327d0cdbb71
Harsh Soni <harsh@192.168.1.4>
2023-05-03 19:17:39 +0530

    add configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/harsh020.github.io/' : '',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
